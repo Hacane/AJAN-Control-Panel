f7f8108b89d475539bc6af0b12c54302a69264fa
Hacane Hechehouche <hacane.hechehouche@dfki.de>
2021-02-01 05:40:26 +0100

    AJAN Control Panel v1.0

--- Ajan/obj/Debug/Ajan_Content.g.i.cs
+++ Ajan/obj/Debug/Ajan_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("3.gif")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("loading.gif")]
 
 
